9469660ba4ffdf4b96c44c12dd0e0d9d5f819a84
Alex Wong <alexw14@gmail.com>
2017-10-06 10:12:14 -0700

    updated roll and calc function

--- js/main.js
+++ js/main.js
@@ -18,46 +18,41 @@ $('.roll').on('click', roll);
 
 // functions
 
-function compute() {                                // compute the result of one rolled pig
+function calc(rand, pts, idx) {                          // calculate the result of one rolled pig
     var result;
-    var rand = Math.floor(Math.random() * 100);    // randomize a number between 0 to 99
+    var pts;
+    var imgIdx;
+    var rand = Math.floor(Math.random() * 100);     // randomize a number between 0 to 99
     if (rand <= 34) {                               // p(Sider Left) = 0.35; range 0-34
-        result = 1;
+        pts = 1;
+        imgIdx = 0;
     } else if (rand > 34 && rand <= 64) {           // p(Sider Right) = 0.30; range 35-64
-        result = 1;
+        pts = 1;
+        imgIdx = 1;
     } else if (rand > 64 && rand <= 84) {           // p(Razorback) = 0.20; range 65-84
-        result = 5;
+        pts = 5;
+        imgIdx = 2;
     } else if (rand > 84 && rand <= 94) {           // p(Trotter) = 0.10; range 85-94
-        result = 5;
+        pts = 5;
+        imgIdx = 3;
     } else if (rand > 94 && rand <= 98) {           // p(Snouter) = 0.04; range 95-98
-        result = 10;
+        pts = 10;
+        imgIdx = 4;
     } else if (rand > 98) {                         // p(Leaning Jowler) = 0.01; range 99
-        result = 15;
+        pts = 15;
+        imgIdx = 5;
     }
-    return result;
+    return result = [rand, pts, imgIdx];            // return an array of [pig position, points]
 }
 
-// when roll is clicked, loop through images
+
 function roll() {
-    // var stopped = true;
-    // var t;
-    var random1 = Math.floor(Math.random() * 6);
-    var random2 = Math.floor(Math.random() * 6);
-    function change() {
-        $('#dice1').attr('src', pigImgs[random1]);
-        $('#dice2').attr('src', pigImgs[random2]);
-    }
-    change();
-    // function stopstart() {
-    //     if (stopped) {
-    //         stopped = false;
-    //         t = setInterval(change, 500);
-    //     } else {
-    //         clearInterval(t);
-    //         stopped = true;
-    //     }
-    // }
-    // stopstart();
+    var d1 = calc();
+    var d2 = calc();
+    console.log(d1);
+    console.log(d2);
+    $('#dice1').attr('src', pigImgs[d1[2]]);
+    $('#dice2').attr('src', pigImgs[d2[2]]);
 }
 
 function init() {
