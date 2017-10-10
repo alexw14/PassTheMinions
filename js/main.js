4c6266f4e04e5f441c9e4b058955c71678c083a8
Alex Wong <alexw14@gmail.com>
2017-10-09 21:33:06 -0700

    updated css

--- js/main.js
+++ js/main.js
@@ -12,16 +12,15 @@ var currentRoundScore = 0;
 var turn;
 
 // images
-imgs[0] = 'images/Pos1_tongue.png'
-imgs[1] = 'images/Pos2_uninterested.png'
-imgs[2] = 'images/Pos3_cupcake.png'
-imgs[3] = 'images/Pos4_happy.png'
-imgs[4] = 'images/Pos5_dancing.png'
-imgs[5] = 'images/Pos6_banana.png'
-imgs[6] = 'images/minion_rotate.png'
+imgs[0] = 'images/minion_rotate.png'
+imgs[1] = 'images/Pos1_tongue.png'
+imgs[2] = 'images/Pos2_uninterested.png'
+imgs[3] = 'images/Pos3_cupcake.png'
+imgs[4] = 'images/Pos4_happy.png'
+imgs[5] = 'images/Pos5_dancing.png'
+imgs[6] = 'images/Pos6_banana.png'
 
 // cached elements
-// var dice = document.querySelectorAll('.dice');
 
 // event listeners
 $('.roll').on('click', roll);
@@ -36,16 +35,12 @@ $('.newgame').on('click', init);
 function rotate() {
     var rollDice1 = $('#dice1').attr('src', 'images/minion_rotate.png');
     var rollDice2 = $('#dice2').attr('src', 'images/minion_rotate.png');
-    // var rollDice1 = $('#dice1').attr('src', pigImgs[0]);
-    // var rollDice2 = $('#dice2').attr('src', pigImgs[1]);
     $('.rotate').css({
         '-webkit-animation-name': 'rotate',
         '-webkit-animation-duration': '0.3s',
         '-webkit-animation-iteration-count': 'infinite',
         '-webkit-animation-timing-function': 'linear',
-    }
-
-    );
+    });
 }
 // stop rotate the images of pig when mouse is released
 function stopRotate() {
@@ -62,38 +57,38 @@ function init() {
     rollScore = 0;
     rollTextDisplay = '';
     currentRoundScore = 0;
-    dice1 = ['', 0, 6];
-    dice2 = ['', 0, 6];
+    dice1 = ['', 0, 0];
+    dice2 = ['', 0, 0];
     render();
 }
 
 function calc() {                                   // calculate the result of one rolled pig
     var result; var pos; var pts; var imgIdx;
     var rand = Math.floor(Math.random() * 100);     // randomize a number between 0 to 99
-    if (rand <= 34) {                               // p(Sider Left) = 0.35; range 0-34
+    if (rand <= 31) {                               // p(Silly) = 0.32; range 0-31
         pos = 'Silly';
         pts = 1;
-        imgIdx = 0;
-    } else if (rand > 34 && rand <= 64) {           // p(Sider Right) = 0.30; range 35-64
+        imgIdx = 1;
+    } else if (rand > 31 && rand <= 63) {           // p(Bored) = 0.32; range 32-63
         pos = 'Bored';
         pts = 1;
-        imgIdx = 1;
-    } else if (rand > 64 && rand <= 84) {           // p(Razorback) = 0.20; range 65-84
+        imgIdx = 2;
+    } else if (rand > 63 && rand <= 79) {           // p(Cupcake) = 0.16; range 64-79
         pos = 'Cupcake';
         pts = 5;
-        imgIdx = 2;
-    } else if (rand > 84 && rand <= 94) {           // p(Trotter) = 0.10; range 85-94
+        imgIdx = 3;
+    } else if (rand > 79 && rand <= 94) {           // p(Happy) = 0.15; range 80-94
         pos = 'Happy';
         pts = 5;
-        imgIdx = 3;
-    } else if (rand > 94 && rand <= 98) {           // p(Snouter) = 0.04; range 95-98
+        imgIdx = 4;
+    } else if (rand > 94 && rand <= 98) {           // p(Hula) = 0.04; range 95-98
         pos = 'Hula'
         pts = 10;
-        imgIdx = 4;
-    } else if (rand > 98) {                         // p(Leaning Jowler) = 0.01; range 99
+        imgIdx = 5;
+    } else if (rand > 98) {                         // p(Banana) = 0.01; range 99
         pos = 'Banana'
         pts = 15;
-        imgIdx = 5;
+        imgIdx = 6;
     }
     return result = [pos, pts, imgIdx];            // return an array of [random number, points, image index]
 }
@@ -113,25 +108,25 @@ function roll() {
 function checkDice() {
     rollScore = 0;      // initial roll score is 0
     if (dice1[2] === dice2[2]) {                    // if dice1 and dice2 are the same
-        if (dice1[2] === 0 || dice1[2] === 1) {     // dice1 and dice2 are the same sider
+        if (dice1[2] === 1 || dice1[2] === 2) {     // dice1 and dice2 are the same sider
             rollScore = 1;                          // score is 1
-            dice1[2] === 0 ? rollTextDisplay = `Double Silly` : rollTextDisplay = `Double Bored`
+            dice1[2] === 1 ? rollTextDisplay = `Double Silly` : rollTextDisplay = `Double Bored`
         } else {                                    // dice1 and dice2 are not sider
             rollScore = (dice1[1] + dice2[1]) * 2;  // score is double pts
             rollTextDisplay = `Double ${dice1[0]}`;
         }
-    } else if (dice1[2] === 0 && dice2[2] === 1) {  // else if dice1 is sider left and dice2 is sider right
+    } else if (dice1[2] === 1 && dice2[2] === 2) {  // else if dice1 is sider left and dice2 is sider right
         rollScore = 0;
         rollTextDisplay = `Pass the Minions!`;
-    } else if (dice1[2] === 1 && dice2[2] === 0) {  // else if dice1 is sider right and dice2 is sider left
+    } else if (dice1[2] === 2 && dice2[2] === 1) {  // else if dice1 is sider right and dice2 is sider left
         rollScore = 0;
         rollTextDisplay = `Pass the Minions!`;
     }
     else if (dice1[2] !== dice2[2]) {               // if two dice are different
-        if (dice1[2] === 0 || dice1[2] === 1) {     // if one dice is a sider and the other is not
+        if (dice1[2] === 1 || dice1[2] === 2) {     // if one dice is a sider and the other is not
             rollScore = Math.max(dice1[1], dice2[1]);   // the sider is worth 0
             rollTextDisplay = `${dice1[0]} and ${dice2[0]}`;
-        } else if (dice2[2] === 0 || dice2[2] === 1) {
+        } else if (dice2[2] === 1 || dice2[2] === 2) {
             rollScore = Math.max(dice1[1], dice2[1]);
             rollTextDisplay = `${dice1[0]} and ${dice2[0]}`;
         }
@@ -187,7 +182,7 @@ function render() {
     $('.newgame').css({'visibility': 'hidden'});
     $('#dice1').attr('src', imgs[dice1[2]]);
     $('#dice2').attr('src', imgs[dice2[2]]);
-    $('.rollscore').html(`${rollTextDisplay} <br> + ${rollScore}`);
+    $('.rollscore').html(`${rollTextDisplay} <br> +${rollScore}`);
     if (currentRoundScore === 0) {
         turn % 2 !== 0 ? $('.display').html(`Player 1's turn!`) : $('.display').html(`Player 2's turn!`);
     } else {
@@ -200,13 +195,3 @@ function render() {
 }
 
 init();
-
-/*
- Notes
- P(Sider_Left)  = 0.35  = 1 pt      range: 0-34
- P(Sider_Right) = 0.30  = 1 pt      range: 35-64
- P(Razorback)   = 0.20  = 5 pts     range: 65-84
- P(Trotter)     = 0.10  = 5 pts     range: 85-94
- P(Snouter)     = 0.04  = 10 pts    range: 95-98
- P(Leaning)     = 0.01  = 15 pts    range: 99
-*/
